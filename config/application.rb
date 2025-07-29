63ed947c8c01447699f4eecefbee5907a40f3974
soham <soham777chavan777@gnail.com>
2025-07-29 19:23:22 +0530

    rm Practice

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,8 @@ Bundler.require(*Rails.groups)
 
 module Practice
   class Application < Rails::Application
-
     config.active_job.queue_adapter = :sidekiq
-    
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
