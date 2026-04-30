70f1493505e10643640601f4d99d85f00bd25192
soham <soham777chavan777@gmail.com>
2026-04-30 19:16:32 +0530

    day 2

---
+++ calculator.rb
@@ -0,0 +1,16 @@
+#calculator
+
+def calculator(a,b,c)
+	case b   
+	when "a+c"
+		puts "+"
+	when "a-c"
+		puts "-"
+	when "a*c"
+		puts "*"
+	when " a/c"
+		puts "/"
+	else 
+		puts "invalid"
+	end
+end
